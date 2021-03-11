885b36bd8c78cf3d03dc5c97d44f8bbe01fa8562
ai-strings <adrstrings@gmail.com>
2021-03-11 23:05:08 +0300

    Installed vuex, json-serv

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const store = createStore({
+})
+
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
