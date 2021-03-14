12978ab786d555f860c768830e4397e03f0b1cc4
ai-strings <adrstrings@gmail.com>
2021-03-14 21:21:51 +0300

    added frontend pagination

--- src/components/tables/vue-table.vue
+++ src/components/tables/vue-table.vue
@@ -10,11 +10,19 @@
     </div>
     <div class="vueTable-body">
       <vueTableRow
-        v-for="row in workersData"
+        v-for="row in paginatedWorkers"
         :key="row.id"
         :rowData = "row"
         :load="log(row)"
       />
+    </div>
+    <div class="vueTable-pagination">
+      <div class="page"
+           v-for="page in pages"
+           :key="page"
+           :class="{'page-active': page === pageNumber}"
+           @click="pageGo(page)"
+      > {{page}} </div>
     </div>
   </div>
 </template>
@@ -35,14 +43,29 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      workersPerPage: 10,
+      pageNumber: 1
+    }
   },
   methods: {
     log (item) {
       console.log(item)
+    },
+    pageGo (page) {
+      this.pageNumber = page
     }
   },
-  computed: {}
+  computed: {
+    pages () {
+      return Math.ceil(this.workersData.length / 10)
+    },
+    paginatedWorkers () {
+      const from = (this.pageNumber - 1) * this.workersPerPage
+      const to = from + this.workersPerPage
+      return this.workersData.slice(from, to)
+    }
+  }
 }
 </script>
 
@@ -57,5 +80,23 @@ export default {
   .vueTable-header span{
     flex-basis: 16.6%;
     padding: 10px 0;
+  }
+  .vueTable-pagination {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 25px;
+  }
+  .page {
+    padding: 8px;
+    margin-right: 5px;
+    border: 2px solid grey;
+  }
+  .page:hover {
+    background: blue;
+    cursor: pointer;
+  }
+  .page-active {
+    background: yellow;
   }
 </style>
